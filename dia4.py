108f7a111328bb9f6a6d1b0b114542854de352f9
Nikolas <nikolassantosleite@gmail.com>
2024-12-14 12:28:42 -0300

    finalizando arquivo dia4

--- dia4.py
+++ dia4.py
@@ -102,3 +102,85 @@ if(ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
 else:
     print(ano, "Não é um ano bissexto")
 
+
+#Caixa eletronico
+
+valor_saque = int(input("Digite o valor do saque: R$ "))
+
+if(valor_saque <= 0):
+    print("Valor inválido")
+else:
+    cedulas_100 = valor_saque // 100
+    valor_saque %= 100
+    cedulas_50 = valor_saque // 50
+    valor_saque %= 50
+    cedulas_20 = valor_saque // 20
+    valor_saque %= 20
+    cedulas_10 = valor_saque // 10
+    valor_saque %= 10
+    cedulas_5 = valor_saque // 5
+    valor_saque %= 5
+    cedulas_2 = valor_saque // 2
+    valor_saque %= 2
+if valor_saque != 0:
+    print("Não é possível sacar o valor especificado com as cédulas disponíveis.")
+else:
+    print("Cédulas entregues:")
+if cedulas_100 > 0:
+    print(f"{cedulas_100} x R$100")
+if cedulas_50 > 0:
+    print(f"{cedulas_50} x R$50")
+if cedulas_20 > 0:
+    print(f"{cedulas_20} x R$20")
+if cedulas_10 > 0:
+    print(f"{cedulas_10} x R$10")
+if cedulas_5 > 0:
+    print(f"{cedulas_5} x R$5")
+if cedulas_2 > 0:
+    print(f"{cedulas_2} x R$2")
+    
+#Emprestimo bancario
+
+valor_emprestimo = float(input("Digite o valor do emprestimo "))
+renda_mensal = float(input("Digite o valor da renda mensal "))
+qtnd_parcelas = int(input("Digite a quantidade de parcelas "))
+
+valor_parcela = valor_emprestimo/qtnd_parcelas
+limite_emprestimo = (renda_mensal * 0.30)
+
+if(valor_emprestimo <=  limite_emprestimo):
+    print("Emprestimo aprovado")
+    print(f"Valor parcela: R${valor_parcela:.2f}")
+else:
+    print("Emprestimo negado")
+    print(f"Valor parcela: R${valor_parcela:.2f} excede 30 % da renda mensal")
+
+
+import random
+
+opções = ["pedra","papel","tesoura"]
+
+escolha_jogador = input("Escolha pedra, papel ou tesoura: ").lower()
+computador = random.choice(opções)
+
+print(f"Voce escolheu: {escolha_jogador}",)
+print(f"O computador escolheu: {computador}")
+
+if(computador == escolha_jogador):
+    print("Empate!")
+elif(escolha_jogador == "papel" and computador == "pedra") or (escolha_jogador == "pedra" and computador == "tesoura") or (escolha_jogador == "tesoura" and computador == "papel"):
+    print("Voce ganhou :)")
+elif escolha_jogador in opções:
+    print("Voce perdeu :(")
+else:
+    print("Escolha inválida")
+    
+
+distancia = int(input("Digite a distancia da corrida em km: "))
+
+tarifa_basica = 4.00
+tarifa_por_km = distancia * 0.25
+valor_total = tarifa_basica + tarifa_por_km
+
+print(f"Valor total da corrida: {valor_total:.2f}",)
+
